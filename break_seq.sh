9f674ab9459241a3f58fe2c2689f0fda5f774a5c
Vincenzo A Ellis <vincenzoaellis@gmail.com>
2018-10-30 13:54:07 +0100

    make default 3000bp

--- break_seq.sh
+++ break_seq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#### Break fasta sequences into sequences of length <= 5000bp so that mappers like
+#### Break fasta sequences into sequences of length <= 3000bp so that mappers like
 #### Stampy can map the fasta reads to a reference (it can't do bigger than 4500bp). 
 #### Rename the sequences sequentially and output to stdout.
 #### Vincenzo A. Ellis
@@ -9,8 +9,8 @@
 # file input
 file_in=${1?Error: no input file}
 
-# maximum sequence length (where to break the sequence, defaults to 5000 bp)
-seq_len=${2:-5000}
+# maximum sequence length (where to break the sequence, defaults to 3000 bp)
+seq_len=${2:-3000}
 
 # do the work
 cat $file_in | sed '1~2d' | fold -w$seq_len | awk '$0=">seq"NR"\n"$0'
